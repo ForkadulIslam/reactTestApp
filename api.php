7676fe34a1f619dc5fc32e51306891cedfa8b288
unknown <forkadulislam@gmail.com>
2024-02-05 23:12:39 +0600

    Add sql and api

--- api.php
+++ api.php
@@ -7,7 +7,7 @@ $dbname = "react_app_test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+$conn->set_charset("utf8");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
